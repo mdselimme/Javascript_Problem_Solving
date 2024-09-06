58f28069b0a901df57314bbfe8b79c6d4d8454f8
mdselimme <mdselimdev@gmail.com>
2024-09-06 08:27:17 +0600

    average_add

---
+++ average_marks_five_sub.js
@@ -0,0 +1,13 @@
+// Subject marks 
+
+var mathMaticsNumber = 75.25;
+var bioLogy = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var totalSubject = 5;
+
+var resultAverage = (mathMaticsNumber+bioLogy+chemistry+physics+bangla)/5;
+
+console.log("Total average marks is : ", resultAverage.toFixed(2));
