cf9f8bb61dda3ed76aa97b601076081f9797e382
mdselimme <mdselimdev@gmail.com>
2024-09-06 08:21:08 +0600

    add-additon-subtraction

---
+++ simple_addition_subtraction.js
@@ -0,0 +1,12 @@
+// First I have money 
+var iHave = 1000;
+
+var apples = 400;
+
+var orange = 300;
+
+var total = apples + orange;
+
+var afterCostiHave = iHave - total;
+
+console.log("After cost I have money : ", afterCostiHave);
