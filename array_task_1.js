64f69957fbbe45ea5819ab657f289ff8033de8ee
mdselimme <mdselimdev@gmail.com>
2024-09-09 23:37:57 +0600

    add-array-task_1

---
+++ array_task_1.js
@@ -0,0 +1,13 @@
+
+//1. Declare an array with 5 elements containing fruits 
+const fruits = ["Orange", "Lemon", "Watermelon", "Jackfruit", "Grapes"];
+
+//console log the 3rd index element
+console.log(fruits[3]);
+
+
+//change the value of the 2nd index element to jambura
+fruits[2] = "Jambura";
+
+//console log the final array
+console.log(fruits);
