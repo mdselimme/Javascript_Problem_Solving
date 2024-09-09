4638601384dde702d97c20ef5e1c67b42ee3db49
mdselimme <mdselimdev@gmail.com>
2024-09-09 23:43:33 +0600

    add_array_second_task

---
+++ array_task_add_remove.js
@@ -0,0 +1,17 @@
+// 1.Declare an array of 3 tourist destinations
+const tourist = ["Dhaka", "Cox's Bazar", "Sylhet"];
+
+// 2.Add a new tourist destination to your tourist array
+tourist.push("Bandarban");
+console.log("1 : " , tourist);
+
+// 3.Add two more to your array
+
+tourist.push("Sunamganj");
+tourist.push("Chattagram");
+console.log("2 : " , tourist);
+
+// 4. Remove the last tourist destination you have added
+tourist.pop();
+
+console.log("3 : " , tourist);
