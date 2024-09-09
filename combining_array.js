d9dd96b688c03e2ddbd4280eabe3f86e99453890
mdselimme <mdselimdev@gmail.com>
2024-09-09 23:56:56 +0600

    array_task_combine

---
+++ combining_array.js
@@ -0,0 +1,11 @@
+// 1.Create two arrays of your choice.
+const arrOne = [1,2,3,4,56,6,4,56];
+const arrTwo = ["Selim", "Sadrul", "Hisan", "Chumka", "Selina"];
+
+// 2.Use the concat method to combine the two arrays into a new array.
+const arrThree = arrOne.concat(arrTwo);
+
+// 3.Print both the original arrays and the combined array using console.log().
+console.log("1 : " , arrOne);
+console.log("2 : " , arrTwo);
+console.log("3 : " , arrThree);
