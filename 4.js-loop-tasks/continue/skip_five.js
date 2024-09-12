8cd1f9cd3e608a45c343f9f9e7036cf188d9862d
mdselimme <mdselimdev@gmail.com>
2024-09-13 01:52:51 +0600

    continue_task_2

---
+++ 4.js-loop-tasks/continue/skip_five.js
@@ -0,0 +1,9 @@
+/*
+display odd number from 55 to 85 and skip the numbers divisible by 5.
+*/
+
+for(let n=55; n<=85; n++){
+    if(n%5===0){
+        console.log("Divisible five number - ", n);
+    }
+}
