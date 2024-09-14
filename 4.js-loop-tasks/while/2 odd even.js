b2407bcbb4299bf70e56e4d9560c0cb9cd6a76e8
mdselimme <mdselimdev@gmail.com>
2024-09-14 18:53:14 +0600

    whilte_task_add

--- 4.js-loop-tasks/while/2 odd even.js
+++ 4.js-loop-tasks/while/2 odd even.js
@@ -6,6 +6,18 @@ Find all the odd numbers from 61 to 100.
 
  */
 
+let od1 = 61;
+let sum1 = 0;
+
+while(od1<=100){
+    if(od1%2!==0){
+        let num1 = sum1;
+        sum1 = sum1+od1;
+        console.log(num1 , " + ",  od1 , " = ", sum1);
+    }
+    od++;
+}
+
 
 /***
 
@@ -15,4 +27,14 @@ Find all the even numbers from 78 to 98.
 
  */
 
-/*programming hero*/
+let od = 78;
+let sum = 0;
+
+while(od<=98){
+    if(od%2==0){
+        let num = sum;
+        sum = sum+od;
+        console.log(num , " + ",  od , " = ", sum);
+    }
+    od++;
+}
